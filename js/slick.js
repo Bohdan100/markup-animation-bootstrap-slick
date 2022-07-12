23f7886aeb4b7be8434f39a5a319ef6e4982b69f
Bohdan <b.orlovskiy@gmail.com>
2022-07-12 23:56:47 +0300

    first commit

---
+++ js/slick.js
@@ -0,0 +1,46 @@
+$(".card-set").slick({
+  autoplay: true,
+  autoplaySpeed: 5000,
+  arrows: true,
+  dots: true,
+  infinite: true,
+
+  speed: 300,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        vertical: true,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        vertical: true,
+      },
+    },
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ],
+});
+
+//   $(".projects__btn-list").slick({
+//     slidesToShow: 3,
+//     slidesToScroll: 1,
+//     autoplay: true,
+//     autoplaySpeed: 2000,
+//   });
